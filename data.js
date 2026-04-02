258c6267cdf54f141a1b28d1c1ef390f2b3d6c90
vuongskysurferstorage <vuongskysurferstorage1@gmail.com>
2026-04-02 07:06:04 -0400

    Update data.js

--- data.js
+++ data.js
@@ -51,7 +51,7 @@ var APP_DATA = {
     },
     {
       "id": "1-2-c-b-i-ving-b-thnh-isave",
-      "name": "二、圣母往见圣妇依撒伯尔",
+      "name": "2. Visitation de Marie à sa Cousine Elisabeth",
       "levels": [
         {
           "tileSize": 256,
@@ -99,14 +99,14 @@ var APP_DATA = {
         {
           "yaw": -0.12524373470416705,
           "pitch": -0.6640726414363716,
-          "title": "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 115%;\">圣母往见圣妇依撒伯尔</span><o:p></o:p></p>",
-          "text": "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">玛利亚就在那几日起身，急速往山区去，到了犹大的一座城。她进了匝加利亚的家，就给依撒伯尔请安。依撒伯尔一听到玛利亚请安，胎儿就在她的腹中欢跃。依撒伯尔遂充满了圣神，大声呼喊说：「在女人中你是蒙祝福的，你的胎儿也是蒙祝福的。吾主的母亲驾临我这里，这是我那里得来的呢？看，你请安的声音一入我耳，胎儿就在我腹中欢喜踊跃。那信了由上主传于她的话必要完成的，是有福的。」<o:p></o:p></span></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">(路加福音 1:39-45)</div>"
+          "title": "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 115%;\">Visitation de Marie à sa Cousine Elisabeth</span><o:p></o:p></p>",
+          "text": "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">En ces jours-là Marie partit et s'en alla en hâte vers la montagne, en une ville de Juda. Et elle entra dans la maison de Zacharie, et salua Elisabeth. Or, quand Elisabeth entendit la salutation de Marie, l'enfant tressaillit dans son sein, et elle fut remplie du Saint-Esprit. Et elle s'écria à haute voix, disant: “ Vous êtes bénie entre les femmes, et le fruit de vos entrailles est béni. Et d'où m'est-il donné que la mère de mon Seigneur vienne à moi? Car votre voix, lorsque vous m'avez saluée, n'a pas plus tôt frappé mes oreilles, que l'enfant a tressailli de joie dans mon sein. Heureuse celle qui a cru! Car elles seront accomplies les choses qui lui ont été dites de la part du Seigneur! ”<o:p></o:p></span></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">(Luc 1:39-45)</div>"
         }
       ]
     },
     {
       "id": "2-3-c-b-sinh-c-cha-gisu-ni-hang-",
-      "name": "三、耶稣基督降生",
+      "name": "3. Naissance de Jésus",
       "levels": [
         {
           "tileSize": 256,
@@ -154,14 +154,14 @@ var APP_DATA = {
         {
           "yaw": -0.16032498761056857,
           "pitch": -0.4172683295906481,
-          "title": "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 115%;\">耶稣基督降生</span><o:p></o:p></p>",
-          "text": "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">那时凯撒奥古斯都出了一道上谕，叫天下的人都要登记：这是在季黎诺作叙利亚总督时，初次行的登记。于是，众人各去本城登记。若瑟因为是达味家族的人，也从加里肋亚纳匝肋城，上犹大名叫白冷的达味城去，好同自己已怀孕的聘妻玛利亚去登记。他们在那里的时候，她分娩的日期满了，便生了她的头胎男儿，用襁褓里起，放在马槽里，因为在客栈中为他们没有地方。</span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">在那地区有些牧人露宿，守夜看守羊群。有上主的一个天使站在他们身边，上主的光耀环照着他们，他们便非常害怕。天使向他们说：「不要害怕！看！我给你们报告一个为全民族的大喜讯：今天在达味城中，为你们诞生了一位救世者，他是主默西亚。这是给你们的记号：你们将要看见一个婴儿，里着襁褓，躺在马槽里。」忽有一大队天军，同那天使一起赞颂天主说：</span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">「天主受享光荣于高天，主爱的人在世享平安！」</span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">众天使离开他们往天上去了以后，牧人们就彼此说：「我们且往白冷去，看看上主报告给我们所发生的事。」他们急忙去了，找到了玛利亚和若瑟，并那躺在马槽中的婴儿。他们看见以后，就把天使对他们论这小孩所说的事，传扬开了，凡听见的人都惊讶牧人向他们所说的事。玛利亚却把这一切事默存在自己心中，反复思想。牧人们为了他们所听见和看见的一切，正如天使向他们说的一样，就光荣赞美天主回去了。<o:p></o:p></span></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">(路加福音 2:1-20)</span></p>"
+          "title": "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 115%;\">Naissance de Jésus</span><o:p></o:p></p>",
+          "text": "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">Or, en ces jours-là, fut publié un édit de César Auguste, pour le recensement de toute la terre. Ce premier recensement eut lieu pendant que Quirinius était gouverneur de Syrie. Et tous allaient se faire recenser, chacun dans sa ville. Joseph aussi monta de Galilée, de la ville de Nazareth, en Judée, à la ville de David, qui s'appelle Bethléem, parce qu'il était de la maison et de la famille de David, pour se faire recenser avec Marie son épouse, qui était enceinte. Or, pendant qu'ils étaient là, le temps où elle devait enfanter s'accomplit, et elle mit au monde son fils premier-né, l'emmaillota et le coucha dans une crèche, parce qu'il n'y avait pas de place pour eux dans l'hôtellerie.</span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">Il y avait dans la même région des bergers qui vivaient aux champs et qui veillaient la nuit sur leur troupeau. Un ange du Seigneur parut auprès d'eux et la gloire du Seigneur les enveloppa de clarté, et ils furent saisis d'une grande crainte. Mais l'ange leur dit: “ Ne craignez point, car je vous annonce une nouvelle qui sera pour tout le peuple une grande joie: il vous est né aujourd'hui, dans la ville de David, un Sauveur, qui est le Christ Seigneur. Et voici ce qui vous en sera le signe: vous trouverez un nouveau-né emmailloté et couché dans une crèche. ” Tout à coup se joignit à l'ange une troupe de la milice céleste, louant Dieu et disant:</span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">“ Gloire, dans les hauteurs, à Dieu! Et, sur terre, paix chez les hommes de bon vouloir! ”</span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">Lorsque les anges, s'en allant au ciel, les eurent quittés, les bergers se dirent entre eux: “ Passons donc jusqu'à Bethléem, et voyons cet événement qui est arrivé, et que le Seigneur nous a fait connaître. ” Ils s'y rendirent en toute hâte, et trouvèrent Marie, Joseph et le nouveau-né couché dans la crèche. Après avoir vu, ils firent connaître ce qui leur avait été dit au sujet de cet enfant. Et tous ceux qui les entendirent furent dans l'admiration de ce que leur avaient dit les bergers. Quant à Maire, elle conservait avec soin toutes ces choses, les méditant dans son coeur. Et les bergers s'en retournèrent, glorifiant et louant Dieu de tout ce qu'il avaient entendu et vu, selon ce qui leur avait été dit.<o:p></o:p></span></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">(Luc 2:1-20)</span></p>"
         }
       ]
     },
     {
       "id": "3-4-c-b-dng-c-cha-gisu-trong-n-thnh",
-      "name": "四、圣母献耶稣于圣殿",
+      "name": "4. Présentation de Jésus au Temple",
       "levels": [
         {
           "tileSize": 256,
@@ -209,14 +209,14 @@ var APP_DATA = {
         {
           "yaw": 0.004950021509621649,
           "pitch": -0.5050909207814644,
-          "title": "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 115%;\">圣母献耶稣于圣殿</span><o:p></o:p></p>",
-          "text": "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">按梅瑟的法律，一满了他们取洁的日期，他们便带着孩子上耶路撒冷去献给上主，就如上主的法律上所记载的：「凡开胎首生的男性，应祝圣于上主。」并该照上主法律上所吩咐的，献上祭物：一对斑鸠或两只鶵鸽。那时，在耶路撒冷有一个人，名叫西默盎。这人正义虔诚，期待着以色列的安慰，而且圣神也在他身上。他曾蒙圣神启示：自己在未看见上主的受傅者以前，决见不到死亡。他因圣神的感动，进了圣殿；那时，抱着婴孩耶稣的父母正进来，要按着法律的惯例为他行礼。西默盎就双臂接过他来，赞美天主说：</span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">「主啊！现在可照你的话，放你的仆人平安去了！因为我亲眼看见了你的救援，即你在万民之前早准备好的：为作启示异邦的光明，你百姓以色列的荣耀。」</span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">他的父亲和母亲就惊异他关于耶稣所说的这些话。西默盎祝福了他们，又向他的母亲玛利亚说：「看，这孩子已被立定，为使以色列中许多人跌倒和复起，并成为反对的记号──至于你，要有一把利剑剌透你的心灵──为叫许多人心中的思念显露出来。」</span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">又有一位女先知亚纳，是阿协尔支派法奴耳的女儿，已上了年纪。她出阁后，与丈夫同居了七年，以后就守寡，直到八十四岁。她斋戒祈祷，昼夜事奉天主，总不离开圣殿。正在那时刻，她也前来称谢天主，并向一切希望耶路撒冷得救赎的人，讲论这孩子。<o:p></o:p></span></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">(路加福音 2:22-38)</span></p>"
+          "title": "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 115%;\">Présentation de Jésus au Temple</span><o:p></o:p></p>",
+          "text": "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">Puis, lorsque les jours de leur purification furent accomplis, selon la loi de Moïse, ils le menèrent à Jérusalem pour le présenter au Seigneur, selon qu'il est écrit dans la loi du Seigneur: Tout mâle premier-né sera regardé comme consacré au Seigneur, et pour offrir en sacrifice, ainsi qu'il est dit dans la loi du Seigneur, une paire de tourterelles ou deux petites colombes. Or, il y avait à Jérusalem un homme nommé Siméon; c'était un homme juste et pieux, qui attendait la consolation d'Israël, et l'Esprit-Saint était sur lui. L'Esprit-Saint lui avait révélé qu'il ne mourrait point avant d'avoir vu le Christ du Seigneur. Il vint donc dans le temple, poussé par l'Esprit. Et comme ses parents amenaient l'enfant Jésus pour observer les coutumes légales à son égard, lui-même le reçut en ses bras, et il bénit Dieu en disant:</span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">“ Maintenant, ô Maître, vous congédiez votre serviteur en paix, selon votre parole; car mes yeux ont vu le salut, que vous avez préparé à la face de tous les peuples, lumière qui doit éclairer les nations et gloire d'Israël, votre peuple. ”</span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">Et son père et sa mère étaient dans l'étonnement pour les choses que l'on disait de lui. Et Siméon les bénit, et il dit à Marie, sa mère: “ Voici qu'il est placé pour la chute et le relèvement d'un grand nombre en Israël, et pour être un signe en butte à la contradiction, - vous-même, un glaive transpercera votre âme, - afin que soient révélées les pensées d'un grand nombre de coeurs. ”</span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">Il y avait aussi une prophétesse, Anne, fille de Phanouel, de la tribu d'Aser; elle était fort avancée en âge, ayant vécu, depuis sa virginité, sept ans avec son mari, et veuve jusqu'à quatre-vingt-quatre ans. Elle ne quittait point le temple, servant Dieu nuit et jour par des jeûnes et des prières. Survenant à cette heure, elle se mit à louer Dieu et à parler de l'enfant à tous ceux qui attendaient la délivrance de Jérusalem.<o:p></o:p></span></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">(Luc 2:22-38)</span></p>"
         }
       ]
     },
     {
       "id": "4-5-c-b-tm-c-c-cha-gisu-trong-n-thnh",
-      "name": "五、耶稣十二龄讲道",
+      "name": "5. Recouvrement de Jésus au Temple",
       "levels": [
         {
           "tileSize": 256,
@@ -258,8 +258,8 @@ var APP_DATA = {
         {
           "yaw": -0.15803943551415855,
           "pitch": -0.41004943600042054,
-          "title": "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 115%;\">耶稣十二龄讲道</span><o:p></o:p></p>",
-          "text": "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">他的父母每年逾越节往耶路撒冷去。他到了十二岁时，他们又照节日的惯例上去了。过完了节日，他们回去的时候，孩童耶稣却留在耶路撒冷，他的父母并未发觉。他们只以为他在同行的人中间，遂走了一天的路程；以后，就在亲戚和相识的人中寻找他。既找不着，便折回耶路撒冷找他。</span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">过了三天，才在圣殿里找到了他。他正坐在经师中，聆听他们，也询问他们。凡听见他的人，对他的智慧和对答，都惊奇不止。他们一看见他，便大为惊异，他的母亲就向他说：「孩子，为什么你这样对待我们？看，你的父亲和我，一直痛苦的找你。」耶稣对他们说：「你们为什么寻找我？你们不知道我必须在我父亲那里吗？」但是，他们不明白他对他们所说的话。<o:p></o:p></span></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">(路加福音 2:41-50)</span></p>"
+          "title": "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 115%;\">Recouvrement de Jésus au Temple</span><o:p></o:p></p>",
+          "text": "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">Or ses parents se rendaient chaque année à Jérusalem, pour la fête de la Pâque. Quand il eut douze ans, comme ils étaient montés selon la coutume de la fête, et qu'ils s'en retournaient, le temps étant passé, l'enfant Jésus resta à Jérusalem et ses parents ne le surent pas. Pensant qu'il était avec la caravane, ils marchèrent tout un jour, puis ils le cherchèrent parmi leurs parents et leurs connaissances. Ne l'ayant point trouvé, ils s'en retournèrent à Jérusalem en le recherchant.</span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">Or, au bout de trois jours, ils le trouvèrent dans le temple, assis au milieu des docteurs, les écoutant et les interrogeant; et tous ceux qui l'entendaient étaient ravis de son intelligence et de ses réponses. En le voyant, ils furent stupéfaits, et sa mère lui dit: “ Mon enfant, pourquoi nous avez-vous fait cela? Voyez, votre père et moi, nous vous cherchions tout affligés. ” Et il leur répondit: “ Pourquoi me cherchiez-vous? Ne saviez-vous pas qu'il me faut être dans les choses de mon Père? ” Mais ils ne comprirent pas la parole qu'il leur dit.<o:p></o:p></span></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">(Luc 2:41-50)</span></p>"
         }
       ]
     }
